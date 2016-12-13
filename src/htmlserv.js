67f05b02aee190304931c601628a5f797e9c0d71
Reynau <xavi.lopez.reynau@gmail.com>
2016-12-13 15:46:11 +0100

    Change console message when server is ready

--- src/htmlserv.js
+++ src/htmlserv.js
@@ -2,11 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+const PORT = 3000
+
 app.use(express.static('dist'))
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../dist/', 'index.html'))
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+  console.log('Game online! Open localhost:' + PORT)
 })
